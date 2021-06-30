fe7a77acb3f7a43facf94baf1b67b5798b482219
Fitsum Maru <fitsumabera24@gmail.com>
2021-06-30 23:08:41 +0000

    fix the errors

--- sentiment.py
+++ sentiment.py
@@ -1,10 +1,10 @@
-import requests 
+import requests
 
 text = input("Enter a text: ")
 
 url = 'http://text-processing.com/api/sentiment/'
 myobj = {'text': text}
 
-response = requests.post(url, data = myobj)
+response = requests.post(url, data=myobj)
 
 print(response.json())
